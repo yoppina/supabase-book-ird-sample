c0e54827ca77038c1d361517f0ad23a58c092de2
yoppina <yoppe0609@gmail.com>
2024-11-17 19:03:23 +0900

    second-commit correct-version

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
+import { Configuration } from "webpack";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config: Configuration) => {
+    config.resolve = {
+      ...config.resolve,
+      modules: [...(config.resolve?.modules || []), path.resolve("./src")],
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+    return (
+        <footer className="px-4 textgray-600 bg-gray-200">
+            <small>&copy; 2024 yoppe.task</small>
+        </footer>
+    );
+};
